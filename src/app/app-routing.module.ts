7a4662b71b0aa7922fae62f31b06ab9d5fe43779
Anukriti <anukriti1512@gmail.com>
2021-10-03 19:28:20 +0530

    logo change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
   { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/login' },
 ];
 
 @NgModule({
