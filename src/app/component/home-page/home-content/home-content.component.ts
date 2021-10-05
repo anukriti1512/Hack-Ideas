e2be8a4f95e0d233689fa5f858536bba7c45f674
Anukriti <anukriti1512@gmail.com>
2021-10-05 16:31:29 +0530

    home page structure changes

--- src/app/component/home-page/home-content/home-content.component.ts
+++ src/app/component/home-page/home-content/home-content.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-content',
   templateUrl: './home-content.component.html',
-  styleUrls: ['./home-content.component.css']
+  styleUrls: ['./home-content.component.css'],
 })
 export class HomeContentComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  data = [
+    { title: 1, tags: ['tech', 'backend'], description: 'My name is anukriti' },
+    {
+      title: 2,
+      tags: ['frontend', 'backend'],
+      description: 'My name is anukriti',
+    },
+    {
+      title: 3,
+      tags: ['tech', 'backend'],
+      description:
+        'My name is anukriti gfdhkjg fndsjkg sjfhjks bgjhsr hgjrhg nfjshgj hgbjdsg fjdsshgkjdhjgds g smgbsn gdsnbgjd gbfjkv sd nngnsd vkjsbgvmdfs gjf vmnfsbgv z gf vmndsbgvzd fdsvmndsbvmcx vbx fxjgv z gbmxvmnsf v cz fmzc  semnvzmnf d v sdv  dzn fmnd vnsbmvd mng fx v dsmvdznbgmd  gnms   vdsjmgmv dzd gbbmfv mabvmzd mgnx c c dsmnvdzbfmd zmvnbdxdm znfvbnz x fvjhnx  zbc mvbfjv xzmnvbvmnx vmn vmcz hgxcn v dm vmndbmv zdvzx nbfmc vmnfsbvmc zmgbvmcgfjxfc,mfkgcxgkjfb,mdfhgbnfddsjkgcngkf bfdmjbgx  jdf cx bgmn v mdrg cx mdss v dbgmmf v,msrbgdv sdbv dmbd v mdnab vmd v dfvdsmg  vx bmjdrbg cxv xnbgjd   dfngn,df v f kjrbv ṣx gjrgcx bmdhtkgjf v sbkjr',
+    },
+    { title: 4, tags: ['fullstack'], description: 'My name is anukriti' },
+    { title: 5, tags: ['sap'], description: 'My name is anukriti' },
+    { title: 6, tags: ['tech', 'backend'], description: 'My name is anukriti' },
+  ];
 }
