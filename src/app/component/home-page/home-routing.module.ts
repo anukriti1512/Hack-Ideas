f94ccc0f14bd74ab0eca198eb9388151535046cd
Anukriti <anukriti1512@gmail.com>
2021-10-14 15:25:40 +0530

    details

--- src/app/component/home-page/home-routing.module.ts
+++ src/app/component/home-page/home-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateHackathonComponent } from './create-hackathon/create-hackathon.component';
+import { HackathonDetailsComponent } from './hackathon-details/hackathon-details.component';
 import { HomeLandingPageComponent } from './home-landing-page/home-landing-page.component';
 import { UserHomeComponent } from './user-home/user-home.component';
 
@@ -11,6 +12,7 @@ const routes: Routes = [
     children: [
       { path: '', component: UserHomeComponent },
       { path: 'create-hackathon', component: CreateHackathonComponent },
+      { path: 'hackathon-details', component: HackathonDetailsComponent },
     ],
   },
 ];
---
+++ src/app/hackathon.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HackathonService {
+  constructor() {}
+
+  HackathonData = [
+    {
+      id: 'H1',
+      title: 'Informatica',
+      tags: ['Data Analytics'],
+      skills: 'informatica powercentre, sql',
+      Rules:
+        'Time duration of Data Warehouse Skills Contest - Informatica would be 45 minutes for MCQ Round ,Participants will have 2 attempts to solve the round, Online webcam proctoring is mandatory to participate in the challenge. The recorded video will be reviewed by the contest administrator,The participant can use any of the following desktop browsers: Internet Explorer 10+, Google Chrome 53+, Mozilla Firefox 60+.,This challenge can not be taken on mobile.You have to complete the rounds in one go without closing/logging out of the coding window. Contest timer will run continuously once you have started attempting the problem.',
+      description:
+        ' We are now looking for talented experts to lead innovation towards business sustainability and revolution.',
+      prize1: 100000,
+      prize2: 75000,
+      endsOn: 'date',
+      createdBy: 'Ashok sharma',
+      creationDate: 'date',
+    },
+    {
+      id: 'H2',
+      title: 'AI/ML Hackathon - Geek Goddess 2021',
+      tags: ['Data Science'],
+      skills: ' Machine Learning / Artificial Intelligence',
+      Rules:
+        'Participants will have 2 attempts to solve the round. Online webcam proctoring is mandatory to participate in the challenge. The recorded video will be reviewed by the contest administrator The participant can use any of the following desktop browsers: Internet Explorer 10+, Google Chrome 53+, Mozilla Firefox 60+.This challenge can not be taken on mobile. You have to complete the round in one go without closing/logging out of the coding window. Contest timer will run continuously once you have started attempting the problem.',
+
+      description:
+        'A subset of machine learning is closely related to computational statistics, which focuses on making predictions using computers; but not all machine learning is statistical learning. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning. Data mining is a related field of study, focusing on exploratory data analysis through unsupervised learning. Some implementations of machine learning use data and neural networks in a way that mimics the working of a biological brain. In its application across business problems, machine learning is also referred to as predictive analytics.',
+      prize1: 100000,
+      prize2: 75000,
+      endsOn: 'date',
+      createdBy: 'Ashok sharma',
+      creationDate: 'date',
+    },
+  ];
+
+  getHackathonData() {
+    return this.HackathonData;
+  }
+}
